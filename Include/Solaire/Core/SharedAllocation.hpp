7fa5205e548025aa1590979d774c352423f6c941
SolaireLibrary <solairelibrary@mail.com>
2016-01-11 13:48:20 +0000

    Reduced memory footprint of SharedAllocation objects

--- Include/Solaire/Core/SharedAllocation.hpp
+++ Include/Solaire/Core/SharedAllocation.hpp
@@ -25,17 +25,34 @@
 	\author
 	Created			: Adam Smith
 	Last modified	: Adam Smith
-	\version 1.0
 	\date
 	Created			: 8th December 2015
-	Last Modified	: 4th January 2015
+	Last Modified	: 11th January 2016
 */
 
-#include "AllocatorI.hpp"
+#include "Solaire/Core/AllocatorI.hpp"
 
 namespace Solaire {
 
-	class Allocator;
+	template<class T>
+	struct SharedObject {
+        AllocatorI& Allocator_;
+        T* Object;
+        uint16_t Count;
+
+        SharedObject(AllocatorI& aAllocator, T* const aObject) :
+            Allocator_(aAllocator),
+            Object(aObject),
+            Count(0)
+        {}
+
+        ~SharedObject() {
+            if(Object) {
+                Object->~T();
+                Allocator_.Deallocate(Object);
+            }
+        }
+	};
 
 	template<class T>
 	class SharedAllocation {
@@ -43,75 +60,52 @@ namespace Solaire {
 		template<class T2>
 		friend class SharedAllocation;
 	private:
-		uint32_t* mCount;
-		AllocatorI* mAllocator;
-		T* mObject;
+		SharedObject<T>* mObject;
 	private:
 		bool DeleteObject() throw() {
-			if(mCount == nullptr) return false;
-			-- *mCount;
-			if(*mCount == 0) {
-				mAllocator->Deallocate(mCount);
-				mCount = nullptr;
-			}else {
-				return false;
-			}
-
-			if(mObject == nullptr) return false;
-			mObject->~T();
-			if(! mAllocator->Deallocate(mObject)) return false;
-			mObject = nullptr;
-
-			return true;
+		    if(mObject == nullptr) return false;
+		    --mObject->Count;
+		    if(mObject->Count == 0) {
+                mObject->~SharedObject();
+                mObject->Allocator_.Deallocate(mObject);
+		    }
+		    mObject = nullptr;
+		    return true;
 		}
 
 	public:
 		SharedAllocation() throw() :
-			mCount(nullptr),
-			mAllocator(nullptr),
 			mObject(nullptr)
 		{}
 
 		SharedAllocation(AllocatorI& aAllocator, T* const aObject) throw() :
-			mCount(new(aAllocator.Allocate(sizeof(uint32_t))) uint32_t(1)),
-			mAllocator(&aAllocator),
-			mObject(aObject)
+			mObject(static_cast<SharedObject<T>>(new(aAllocator.Allocate(sizeof(SharedObject<T>))) SharedObject<T>(aAllocator, aObject)))
 		{}
 
 		SharedAllocation(const SharedAllocation<T>& aOther) throw() :
-			mCount(aOther.mCount),
-			mAllocator(aOther.mAllocator),
 			mObject(aOther.mObject)
 		{
-			if(mCount)++ *mCount;
+			if(mObject) ++mObject->Object;
 		}
 
 		SharedAllocation(SharedAllocation<T>&& aOther) throw() :
-			mCount(aOther.mCount),
-			mAllocator(aOther.mAllocator),
 			mObject(aOther.mObject)
 		{
 			aOther.mObject = nullptr;
-			aOther.mCount = nullptr;
 		}
 
 		template<class T2>
 		SharedAllocation(const SharedAllocation<T2>& aOther) throw() :
-			mCount(aOther.mCount),
-			mAllocator(aOther.mAllocator),
 			mObject(aOther.mObject)
 		{
-			++ *mCount;
+			++mObject->Count;
 		}
 
 		template<class T2>
 		SharedAllocation(SharedAllocation<T2>&& aOther) throw() :
-			mCount(aOther.mCount),
-			mAllocator(aOther.mAllocator),
 			mObject(aOther.mObject)
 		{
 			aOther.mObject = nullptr;
-			aOther.mCount = nullptr;
 		}
 
 		~SharedAllocation() throw() {
@@ -120,10 +114,8 @@ namespace Solaire {
 
 		SharedAllocation& operator=(const SharedAllocation<T>& aOther) throw() {
 			DeleteObject();
-			mCount = aOther.mCount;
-			mAllocator = aOther.mAllocator;
 			mObject = aOther.mObject;
-			++ *mCount;
+			++mObject->Count;
 			return *this;
 		}
 
@@ -135,36 +127,30 @@ namespace Solaire {
 		template<class T2>
 		SharedAllocation& operator=(const SharedAllocation<T2>& aOther) throw() {
 			DeleteObject();
-			mCount = aOther.mCount;
-			mAllocator = aOther.mAllocator;
 			mObject = aOther.mObject;
-			++ *mCount;
+			++mObject->Count;
 			return *this;
 		}
 
 		void Swap(SharedAllocation<T>& aOther) throw() {
-			std::swap(mAllocator, aOther.mAllocator);
 			std::swap(mObject, aOther.mObject);
-			std::swap(mCount, aOther.mCount);
 		}
 
 		T* ReleaseOwnership() throw() {
-			if(mCount == nullptr) return nullptr;
-			if(*mCount != 1) return nullptr;
+			if(mObject == nullptr) return nullptr;
+			if(mObject->Count != 1) return nullptr;
 
-			T* const tmp = mObject;
-			mAllocator->Deallocate(mCount);
-			mCount = nullptr;
-			mObject = nullptr;
+			T* const tmp = mObject->Object;
+			DeleteObject();
 			return tmp;
 		}
 
 		AllocatorI& GetAllocator() const throw() {
-			return *mAllocator;
+			return mObject->Allocator_;
 		}
 
 		uint32_t GetUserCount() const throw() {
-			return mCount == nullptr ? 0 : *mCount;
+			return mObject == nullptr ? 0 : mObject->Count;
 		}
 
 		operator bool() const throw() {
@@ -172,11 +158,11 @@ namespace Solaire {
 		}
 
 		T& operator*() const throw() {
-			return *mObject;
+			return *(mObject->Object);
 		}
 
 		T* operator->() const throw() {
-			return mObject;
+			return mObject == nullptr ? nullptr : mObject->Object;
 		}
 	};
 }
