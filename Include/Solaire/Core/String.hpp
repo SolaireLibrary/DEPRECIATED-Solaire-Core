6334197565b5f4fa4c0af9af39a75b0d66a1195e
SolaireLibrary <solairelibrary@mail.com>
2016-01-18 13:10:13 +0000

    Better implementation of string > numeric conversion

--- Include/Solaire/Core/String.hpp
+++ Include/Solaire/Core/String.hpp
@@ -37,7 +37,104 @@ namespace Solaire {
     template<class T>
 	using StringConstant = StaticContainer<T>;
 
+    namespace Implementation {
+        static constexpr char STRING_NUMERIC_CHARS[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+        static constexpr uint64_t constexprPower(const uint64_t aValue, const uint64_t aExponent) {
+            return aExponent == 0 ? 1 : aExponent == 1 ? aValue : aValue * constexprPower(aValue, aExponent - 1);
+        }
+
+        static constexpr uint16_t POWERS_OF_10[] = {
+            constexprPower(10, 0),
+            constexprPower(10, 1),
+            constexprPower(10, 2),
+            constexprPower(10, 3),
+            constexprPower(10, 4),
+            constexprPower(10, 5),
+            constexprPower(10, 6),
+            constexprPower(10, 7),
+            constexprPower(10, 8),
+            constexprPower(10, 9),
+            constexprPower(10, 10),
+            constexprPower(10, 11),
+            constexprPower(10, 12),
+            constexprPower(10, 13),
+            constexprPower(10, 14)
+        };
+
+        static uint64_t stringToUnsigned(const char* const aString, const int32_t aSize) throw() {
+            //! \todo Support different bases
+            int32_t begin = 0;
+            while(begin < aSize && aString[begin] == '0') {
+                ++begin;
+            }
+
+            uint64_t value = 0;
+            uint8_t col = 0;
+            for(uint32_t i = begin; i < aSize; ++i, ++col) {
+                const char c = aString[aSize - (i + 1)];
+                if(c < '0' || c > '9') break;
+                value += static_cast<uint64_t>(c - '0') * Implementation::POWERS_OF_10[col];
+            }
+
+            return value;
+        };
+
+        static int64_t stringToSigned(const char* const aString, const int32_t aSize) throw() {
+            //! \bug Does not parse last column
+            //! \todo Support different bases
+            bool sign = false;
+            int32_t begin = 0;
+            if(begin < aSize && aString[begin] == '-') {
+                sign = true;
+                ++begin;
+            }
+            while(begin < aSize && aString[begin] == '0') {
+                ++begin;
+            }
+
+            int64_t value = 0;
+            uint8_t col = 0;
+            for(uint32_t i = begin; i < aSize; ++i, ++col) {
+                const char c = aString[aSize - (i + 1)];
+                if(c < '0' || c > '9') break;
+                value += static_cast<int64_t>(c - '0') * Implementation::POWERS_OF_10[col];
+            }
+
+            return sign ? value * -1 : value;
+        }
+
+        static int64_t stringToDouble(const char* const aString, const int32_t aSize) throw() {
+            //! \todo Support different bases
+            int32_t decimal = -1;
+            for(int32_t i = 0; i < aSize; ++i) {
+                if(aString[i] == '.') {
+                    decimal = i;
+                }
+            }
+
+            const int32_t high = stringToSigned(aString, decimal);
+            const uint32_t low = stringToUnsigned(aString + decimal + 1, aSize - (decimal + 1));
+
+            double lowD = static_cast<double>(low);
+            while(lowD < 0.0) lowD /= 10.0;
+            return static_cast<double>(high) + lowD;
+        }
+
+        thread_local char CSTRING_BUF[1024];
+
+        template<class T>
+        static const char* stringToCString(const StringConstant<T>& aString) throw() {
+            if(aString.isContiguous() && (std::is_same<T, char>::value || std::is_same<T, const char>::value)) {
+                return &aString[0];
+            }else {
+                char* ptr = CSTRING_BUF;
+                const auto end = aString.end();
+                for(auto i = aString.begin(); i != end; ++i, ++ptr) *ptr = *i;
+                return CSTRING_BUF;
+            }
+        }
+	}
 
     template<class T>
 	class StringFragment : public StringConstant<T> {
@@ -129,6 +226,48 @@ namespace Solaire {
             return *this;
         }
 
+        explicit operator uint64_t() const throw() {
+            //! \todo Support different bases
+            return Implementation::stringToUnsigned(Implementation::stringToCString(*this), this->size());
+        }
+
+        explicit operator uint32_t() const throw() {
+            return static_cast<uint32_t>(static_cast<uint64_t>(*this));
+        }
+
+        explicit operator uint16_t() const throw() {
+            return static_cast<uint16_t>(static_cast<uint64_t>(*this));
+        }
+
+        explicit operator uint8_t() const throw() {
+            return static_cast<uint8_t>(static_cast<uint64_t>(*this));
+        }
+
+        explicit operator int64_t() const throw() {
+            //! \todo Support different bases
+            return Implementation::stringToSigned(Implementation::stringToCString(*this), this->size());
+        }
+
+        explicit operator int32_t() const throw() {
+            return static_cast<int32_t>(static_cast<int64_t>(*this));
+        }
+
+        explicit operator int16_t() const throw() {
+            return static_cast<int16_t>(static_cast<int64_t>(*this));
+        }
+
+        explicit operator int8_t() const throw() {
+            return static_cast<int8_t>(static_cast<int64_t>(*this));
+        }
+
+        explicit operator float() const throw() {
+            return static_cast<float>(static_cast<double>(*this));
+        }
+
+        explicit operator double() const throw() {
+            return Implementation::stringToDouble(Implementation::stringToCString(*this), this->size());
+        }
+
         // Inherited from StaticContainer
 
         bool SOLAIRE_EXPORT_CALL isContiguous() const throw() override {
@@ -144,32 +283,6 @@ namespace Solaire {
         }
 
 	};
-
-	namespace Implementation {
-        static constexpr char STRING_NUMERIC_CHARS[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-        static constexpr uint64_t constexprPower(const uint64_t aValue, const uint64_t aExponent) {
-            return aExponent == 0 ? 1 : aExponent == 1 ? aValue : aValue * constexprPower(aValue, aExponent - 1);
-        }
-
-        static constexpr uint16_t POWERS_OF_10[] = {
-            constexprPower(10, 0),
-            constexprPower(10, 1),
-            constexprPower(10, 2),
-            constexprPower(10, 3),
-            constexprPower(10, 4),
-            constexprPower(10, 5),
-            constexprPower(10, 6),
-            constexprPower(10, 7),
-            constexprPower(10, 8),
-            constexprPower(10, 9),
-            constexprPower(10, 10),
-            constexprPower(10, 11),
-            constexprPower(10, 12),
-            constexprPower(10, 13),
-            constexprPower(10, 14)
-        };
-	}
 
     template<class T>
 	SOLAIRE_EXPORT_INTERFACE String : public List<T> {
@@ -251,21 +364,7 @@ namespace Solaire {
 
         explicit operator uint64_t() const throw() {
             //! \todo Support different bases
-            int32_t begin = 0;
-            const int32_t end = this->size();
-            while(begin < end && this->operator[](begin) == '0') {
-                ++begin;
-            }
-
-            uint64_t value = 0;
-            uint8_t col = 0;
-            for(uint32_t i = begin; i < end; ++i, ++col) {
-                const char c = this->operator[](end - (i + 1));
-                if(c < '0' || c > '9') break;
-                value += static_cast<uint64_t>(c - '0') * Implementation::POWERS_OF_10[col];
-            }
-
-            return value;
+            return Implementation::stringToUnsigned(Implementation::stringToCString(*this), this->size());
         }
 
         explicit operator uint32_t() const throw() {
@@ -281,28 +380,8 @@ namespace Solaire {
         }
 
         explicit operator int64_t() const throw() {
-            //! \bug Does not parse last column
             //! \todo Support different bases
-            bool sign = false;
-            int32_t begin = 0;
-            const int32_t end = this->size();
-            if(begin < end && this->operator[](begin) == '-'){
-                sign = true;
-                ++begin;
-            }
-            while(begin < end && this->operator[](begin) == '0') {
-                ++begin;
-            }
-
-            int64_t value = 0;
-            uint8_t col = 0;
-            for(uint32_t i = begin; i < end; ++i, ++col) {
-                const char c = this->operator[](end - (i + 1));
-                if(c < '0' || c > '9') break;
-                value += static_cast<int64_t>(c - '0') * Implementation::POWERS_OF_10[col];
-            }
-
-            return sign ? value * -1 : value;
+            return Implementation::stringToSigned(Implementation::stringToCString(*this), this->size());
         }
 
         explicit operator int32_t() const throw() {
@@ -315,6 +394,14 @@ namespace Solaire {
 
         explicit operator int8_t() const throw() {
             return static_cast<int8_t>(static_cast<int64_t>(*this));
+        }
+
+        explicit operator float() const throw() {
+            return static_cast<float>(static_cast<double>(*this));
+        }
+
+        explicit operator double() const throw() {
+            return Implementation::stringToDouble(Implementation::stringToCString(*this), this->size());
         }
 
 	};
