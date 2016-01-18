e10793d94ac9f9910629aa4f3d89ec9738ed64f9
SolaireLibrary <solairelibrary@mail.com>
2016-01-18 12:18:35 +0000

    Moved StringFragment from Data module

--- Include/Solaire/Core/String.hpp
+++ Include/Solaire/Core/String.hpp
@@ -27,7 +27,7 @@
 	Last modified	: Adam Smith
 	\date
 	Created			: 10th December 2015
-	Last Modified	: 15th January 2016
+	Last Modified	: 18th January 2016
 */
 
 #include "Solaire/Core/Container.hpp"
@@ -36,6 +36,114 @@ namespace Solaire {
 
     template<class T>
 	using StringConstant = StaticContainer<T>;
+
+
+
+    template<class T>
+	class StringFragment : public StringConstant<T> {
+	public:
+		typedef char Type;
+		typedef const char ConstType;
+		typedef Type& Reference;
+		typedef ConstType& ConstReference;
+		typedef Type&& Move;
+		typedef Type* Pointer;
+		typedef ConstType* ConstPointer;
+		typedef uint32_t Index;
+		typedef StringFragment<T> Self;
+	private:
+	    StringConstant<T>* mString;
+	    int32_t mBegin;
+	    int32_t mEnd;
+    protected:
+        // Inherited from StaticContainer
+
+        Pointer SOLAIRE_EXPORT_CALL getPtr(int32_t aOffset) throw() override {
+            return mString->getPtr(mBegin + aOffset);
+        }
+
+        SharedAllocation<Iterator<Type>> SOLAIRE_EXPORT_CALL begin_() throw() override {
+            SharedAllocation<Iterator<Type>> it = mString->begin_();
+            it->increment(mBegin);
+            return it;
+        }
+
+        SharedAllocation<Iterator<Type>> SOLAIRE_EXPORT_CALL end_() throw() override {
+            SharedAllocation<Iterator<Type>> it = mString->begin_();
+            it->increment(mEnd);
+            return it;
+        }
+
+        SharedAllocation<Iterator<Type>> SOLAIRE_EXPORT_CALL rbegin_() throw() override {
+            SharedAllocation<Iterator<Type>> it = mString->rbegin();
+            it->increment(mString.size() - mEnd);
+            return it;
+        }
+
+        SharedAllocation<Iterator<Type>> SOLAIRE_EXPORT_CALL rend_() throw() override {
+            SharedAllocation<Iterator<Type>> it = mString->rbegin();
+            it->increment(mString.size() - mBegin);
+            return it;
+        }
+
+    public:
+        StringFragment(StringConstant<T>& aString, const int32_t aBegin) :
+            mString(&aString),
+            mBegin(aBegin),
+            mEnd(aString.size())
+        {}
+
+        StringFragment(StringConstant<T>& aString, const int32_t aBegin, const int32_t aEnd) :
+            mString(&aString),
+            mBegin(aBegin),
+            mEnd(aEnd)
+        {}
+
+        StringFragment(const Self& aOther) :
+            mString(aOther.mString),
+            mBegin(aOther.mBegin),
+            mEnd(aOther.mEnd)
+        {}
+
+        StringFragment(Self&& aOther) :
+            mString(aOther.mString),
+            mBegin(aOther.mBegin),
+            mEnd(aOther.mEnd)
+        {}
+
+        SOLAIRE_EXPORT_CALL ~StringFragment() {
+
+        }
+
+        Self& operator=(const Self& aOther) {
+            mString = aOther.mString;
+            mBegin = aOther.mBegin;
+            mEnd = aOther.mEnd;
+            return *this;
+        }
+
+        Self& operator=(Self&& aOther) {
+            mString = aOther.mString;
+            mBegin = aOther.mBegin;
+            mEnd = aOther.mEnd;
+            return *this;
+        }
+
+        // Inherited from StaticContainer
+
+        bool SOLAIRE_EXPORT_CALL isContiguous() const throw() override {
+            return mString->isContiguous();
+        }
+
+        int32_t SOLAIRE_EXPORT_CALL size() const throw() override {
+            return mEnd - mBegin;
+        }
+
+        Allocator& SOLAIRE_EXPORT_CALL getAllocator() const throw() override {
+            return mString->getAllocator();
+        }
+
+	};
 
 	namespace Implementation {
         static constexpr char STRING_NUMERIC_CHARS[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -67,6 +175,15 @@ namespace Solaire {
 	SOLAIRE_EXPORT_INTERFACE String : public List<T> {
     public:
         SOLAIRE_EXPORT_CALL ~String() throw() {}
+
+        template<size_t LENGTH>
+        String<T>& operator=(const T(&aString)[LENGTH]) {
+            this->clear();
+            for(int32_t i = 0; i < LENGTH-1; ++i) this->pushBack(aString[i]);
+            const char c = aString[LENGTH-1];
+            if(c != '\0') this->pushBack(c);
+            return *this;
+        }
 
         template<size_t LENGTH>
         String<T>& operator+=(const T(&aString)[LENGTH]) throw() {
